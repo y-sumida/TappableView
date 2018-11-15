d1fee67c819cc9a1e79a5962420b10c956579d65
Yuki Sumida <y.sumida.0201@gmail.com>
2018-11-15 23:49:19 +0900

    ボタンのタイトルがセットされていても無視する

--- TappableView/InvisibleButton.swift
+++ TappableView/InvisibleButton.swift
@@ -10,4 +10,28 @@ class InvisibleButton: UIButton {
             backgroundColor = isHighlighted ? hilightedColor : UIColor.clear
         }
     }
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        commonInit()
+    }
+
+    private func commonInit() {
+        super.setTitle(nil, for: .normal)
+        super.setTitle(nil, for: .highlighted)
+    }
+
+    override func setTitle(_ title: String?, for state: UIControl.State) {
+        // 無効化する
+    }
 }
