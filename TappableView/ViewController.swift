2b016f44965a2b6f6335a5090d7553ded23161d7
Yuki Sumida <y.sumida.0201@gmail.com>
2018-11-15 23:31:04 +0900

    プロジェクト生成

---
+++ TappableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TappableView
+//
+//  Created by Yuki Sumida on 2018/11/15.
+//  Copyright © 2018年 Yuki Sumida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
